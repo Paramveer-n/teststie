afd0a3d9adc2f2ae40079d0b8429c01d33ce7ec6
Paramveer-n <44220416+Paramveer-n@users.noreply.github.com>
2022-01-18 12:24:48 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-import initScrollReveal from "./scripts/scrollReveal";
-import initTiltEffect from "./scripts/tiltAnimation";
-import { targetElements, defaultProps } from "./data/scrollRevealConfig";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-initScrollReveal(targetElements, defaultProps);
-initTiltEffect();
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
